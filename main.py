8cb8756e99f4180ba72d68e9512e59d90c2ad893
ichika123 <inakano514@gmail.com>
2025-05-26 10:48:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import discord
 import os
+import random
+import asyncio
 from discord.ext import commands
 from keep_alive import keep_alive
+
+
+
 
 intents = discord.Intents.default()
 prefix = '-'
@@ -14,11 +19,19 @@ bot = commands.Bot(command_prefix=prefix,
                    self_bot=True,
                    intents=intents)
 
+async def update_presence():
+    statuses = [discord.Status.online, discord.Status.idle, discord.Status.dnd]
+    while True:
+        await bot.change_presence(status=random.choice(statuses))
+        await asyncio.sleep(random.randint(1800, 7200))  # Change status every 30-120 minutes
+
+
 
 @bot.event
-async def on_ready():
-    await bot.change_presence(status=discord.Status.dnd)
-    print(f'Onliner installed in ' + bot.user.name)
+async def on_message(message):
+    if message.author.id != bot.user.id:  # Ignore self messages
+        await bot.typing()
+        await asyncio.sleep(random.randint(2, 5))  # Mimic human response time
 
 keep_alive()
 bot.run(token, bot=False)
